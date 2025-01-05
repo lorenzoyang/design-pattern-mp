dd749e7d78d969b5a37773cd71700693270dddbf
lorenzoyang <yangrongwei2002@gmail.com>
2025-01-05 23:36:29 +0100

    fixed

--- src/main/java/com/github/lorenzoyang/streamingplatform/user/AccessDeniedException.java
+++
@@ -1,7 +0,0 @@
-package com.github.lorenzoyang.streamingplatform.user;
-
-public class AccessDeniedException extends RuntimeException {
-    public AccessDeniedException(String message) {
-        super(message);
-    }
-}
